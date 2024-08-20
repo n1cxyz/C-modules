de7444cb9e115b6803de468b031c9f0155f54058
Dominic Asal <dasal@c4a9c5.42berlin.de>
2024-08-20 09:08:06 +0200

    added header

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dasal <dasal@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/20 09:05:17 by dasal             #+#    #+#             */
-/*   Updated: 2024/08/20 09:05:18 by dasal            ###   ########.fr       */
+/*   Created: 2024/08/20 09:06:20 by dasal             #+#    #+#             */
+/*   Updated: 2024/08/20 09:06:21 by dasal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
 
-Zombie::Zombie() {
-	std::cout <<  "a new zombie has arrived" << "\n";
-};
-
-Zombie::~Zombie() {
-	std::cout << name << " died... miserably." << "\n";
-};
-        
-void    Zombie::announce(void) {
-	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << "\n";
-};
-
-void	Zombie::setName(std::string name) {
-	this->name = name;
-}
