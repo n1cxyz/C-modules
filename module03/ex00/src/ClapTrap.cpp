cd94cc67ed71fa9f9a17c2d1dc6d66322976d11a
n1cxyz <owerwa@gmail.com>
2024-08-21 23:23:09 +0200

    added module03

---
+++ module03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,22 @@
+#include "ClapTrap.hpp"
+
+/* class ClapTrap {
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& claptrap);
+		ClapTrap& operator=(const ClapTrap& claptrap);
+		~ClapTrap();
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+	private:
+		std::string	name;
+		int			hitPoints;
+		int			energyPoints;
+		int			attackDamage;		
+}; */
+
+ClapTrap::ClapTrap(std::string name) {
+
+}
